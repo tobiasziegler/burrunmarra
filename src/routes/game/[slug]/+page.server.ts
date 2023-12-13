cac0ddcbc597a72da4073aa60e2e27f1bdc7d41d
David Mallard <correspondence@davidmallard.id.au>
2023-12-13 18:34:54 +1100

    Add working functionality to reset to a new game round

--- src/routes/game/[slug]/+page.server.ts
+++ src/routes/game/[slug]/+page.server.ts
@@ -5,11 +5,13 @@ import type { Game } from '$lib/games';
 export const actions = {
 	default: async ({ request }) => {
 		const data = await request.formData();
+		const reset = data.get('reset');
 		const select = data.get('select');
 		const oldGame = data.get('game');
 		const game: Game = JSON.parse(oldGame);
 
 		return {
+			reset,
 			select,
 			game
 		};
--- src/routes/game/[slug]/+page.svelte
+++ src/routes/game/[slug]/+page.svelte
@@ -1,19 +1,13 @@
 <script lang="ts">
 	import type { PageData, ActionData } from './$types';
-	import type { Game } from '$lib/games';
 
 	import { enhance } from '$app/forms';
 
 	export let data: PageData;
 	export let form: ActionData;
 
-	let game: Game;
-
-	if (form) {
-		game = form.game;
-	} else {
-		game = data.game;
-	}
+	$: select = form?.select;
+	$: game = select ? form.game : data.game;
 </script>
 
 <form method="POST" use:enhance>
@@ -21,16 +15,16 @@
 
 	<div class="images">
 		{#each game.items as item}
-			<button name="select" value={item.wiradjuri} disabled={form?.select ? true : false}>
+			<button name="select" value={item.wiradjuri} disabled={select ? true : false}>
 				<img src={`/img/${item.image}`} alt={`${item.english}`} />
 			</button>
 		{/each}
 		<input type="hidden" name="game" value={JSON.stringify(game)} />
 	</div>
 
-	{#if form?.select}
+	{#if select}
 		<p class="answer">
-			You selected {form.select} - {form.select == game.target ? 'correct! ✅' : 'incorrect ❌'}
+			You selected {form.select} - {select == game.target ? 'correct! ✅' : 'incorrect ❌'}
 		</p>
 		<button name="reset" value="true" class="button">Next Pick</button>
 	{/if}
